1c19c4b1c3b1e792fc6a5bbf58c30519b867b347
Abir835 <abirhasan.raj.bd@gmail.com>
2022-01-24 00:23:42 +0600

    date time operator loop learning

---
+++ Ch-1/math.js
@@ -0,0 +1,23 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 10.326
+
+console.log(Math.abs(n))
+console.log(Math.ceil(n))
+console.log(Math.floor(n))
+console.log(Math.round(n))
+console.log(Math.max(400, 500))
+
+console.log(Math.min(400, 500))
+
+//squareroot
+
+console.log(Math.sqrt(9))
+//power
+
+console.log(Math.pow(2, 3))
+
+//random number
+
+console.log(Math.round(Math.random() * 50 + 1))
---
+++ Ch-1/ternary.js
@@ -0,0 +1,9 @@
+var a = 10
+
+console.log(a%2==0 ? "Even": "ODD")
+var name = 'Hello'
+
+var fullName = name || 'Abir Hasan'
+console.log(fullName)
+ok = true
+ok && console.log("ok")
